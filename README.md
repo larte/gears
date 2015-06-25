bf13169390ae04d6e2f364b2aeed2993eefd1ed1
larte <lauri.arte@gmail.com>
2015-06-25 13:51:06 +0300

    Some formatting and readme.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Gears
+
+A simple tool to do just one thing: Compare gear ratios for track cycling.
+Intended to be usable on all browsers and platforms.
+
+
+## About the calculations
+
+Tire size is 700 x 22c, with a 2092 mm ( or 26.215 inches ) diameter.
--- js/gears.js
+++ js/gears.js
@@ -1,14 +1,15 @@
+"use strict";
 function inches(cogsize, chsize) {
-    return 26.216 * ( chsize / cogsize);
-};
+    return 26.216 * (chsize / cogsize);
+}
 
 function ratio(cogsize, chsize) {
     return chsize / cogsize;
-};
+}
 
 function kmh(cogsize, chsize, cad) {
     var rat = ratio(cogsize, chsize);
-    var res = 0.0000254 * (cad * (rat * ( 26.216 * Math.PI)) * 60.0);
+    var res = 0.0000254 * (cad * (rat * (26.216 * Math.PI)) * 60.0);
     return res;
 
 }
@@ -20,31 +21,26 @@ function tt(cogsize, chsize, cad) {
 
 var app = angular.module('gears', ['ui.bootstrap','rzModule']);
 
-app.controller('Main', function($rootScope,$scope,$modal){
+app.controller('Main', function ($rootScope, $scope, $modal){
 
     $scope.chainSlider = 50;
     $scope.cogSlider = 15;
     $scope.cadenceSlider = 100;
 
-    $scope.calculate = function() {
+    $scope.calculate = function () {
         var cr = parseFloat($scope.chainSlider);
         var cog = parseFloat($scope.cogSlider);
         var cad = parseFloat($scope.cadenceSlider);
 
-        var inc   = inches(cog, cr).toFixed(2);
-        var rat = ratio(cog, cr).toFixed(2);
-        var speed = kmh(cog, cr, cad).toFixed(2);
-        var tt2   = tt(cog, cr, cad);
+        $scope.gearInches = inches(cog, cr).toFixed(2);
+        $scope.gearRatio  = ratio(cog, cr).toFixed(2);
+        $scope.gearSpeedd = kmh(cog, cr, cad).toFixed(2);
+        $scope.gearTt     = tt(cog, cr, cad);
 
-        $scope.gearInches = inc;
-        $scope.gearRatio = rat;
-        $scope.gearSpeed = speed;
-        $scope.gearTt = tt2;
         $scope.$apply();
     };
 
     $scope.$on("slideEnded", function() {
-        //alert("Slide ended" + $scope.chainSlider);
         $scope.calculate();
     });
 
