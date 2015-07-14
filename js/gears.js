e0e7d7459960fe5b1eb924e2af68c8501b35829a
larte <lauri.arte@gmail.com>
2015-07-14 09:50:38 +0300

    Added equivalent gears and removed extra results.

--- js/gears.js
+++ js/gears.js
@@ -13,51 +13,84 @@ String.prototype.toMMSS = function () {
     return time;
 };
 
+
+var Gear = function(cogsize, chainringsize){
+    var self = this;
+    this.cog = cogsize;
+    this.ch  = chainringsize;
+
+    self.inches = function() {
+        return 26.216 * (self.ch / self.cog);
+    }
+
+    self.ratio = function() {
+        return (self.ch / self.cog);
+    }
+
+    self.speed_at = function(cad) {
+        return (0.0000254 * (cad * ( self.ratio() * (26.216 * Math.PI)) * 60.0));
+    }
+
+
+    self.time = function(cad, dist) {
+        var sec = (dist / (self.speed_at(cad) / 3.6)).toFixed(3);
+        if (sec >= 60.0)
+            return sec.toMMSS();
+        return sec;
+    }
+
+
+    self.equivalent = function(cogsize, chsize) {
+        var rat = self.ratio();
+        var res = {"up":   {"ratio":rat + 1.0, "cog": 0, "chainring": 0},
+                   "down": {"ratio":rat - 1.0, "cog": 0, "chainring": 0}};
+
+        var erat = 0.0;
+        for (var cog = 11; cog  < 17; cog++){
+            for (var ch = 47; ch < 60; ch++) {
+                var eg = new Gear(cog,ch);
+                erat = eg.ratio();
+                if ( (erat < rat) && ( erat > res["down"]["ratio"] ) ){
+                    res["down"] = {"ratio": erat, inches: eg.inches().toFixed(2),
+                                   "cog": cog, "chainring": ch}
+                }
+                else if ( (erat > rat) && (erat < res["up"]["ratio"]) ){
+                    res["up"] = {"ratio": erat, inches: eg.inches().toFixed(2),
+                                 "cog": cog, "chainring": ch}
+                }
+
+            }
+        }
+        return res;
+    }
+
+    return self;
+};
+
+
+
 var app = angular.module('gears', ['ui.bootstrap','rzModule']);
 
 app.controller('Main', function ($rootScope, $scope, $modal){
 
     $scope.chainSlider = 50;
-    $scope.cogSlider = 15;
+    $scope.cogSlider = 13;
     $scope.cadenceSlider = 100;
-
-
-    $scope.inches = function(cogsize, chsize) {
-        return 26.216 * (chsize / cogsize);
-    };
-
-    $scope.ratio = function(cogsize, chsize) {
-        return chsize / cogsize;
-    };
-
-    $scope.kmh = function(cogsize, chsize,cad) {
-        var rat = $scope.ratio(cogsize, chsize);
-        var res = 0.0000254 * (cad * (rat * (26.216 * Math.PI)) * 60.0);
-        return res;
-    };
-
-
-    $scope.tt = function(dist, cogsize, chsize, cad) {
-        var speed = $scope.kmh(cogsize, chsize, cad);
-        var sec = (dist / (speed / 3.6)).toFixed(3);
-        if (sec >= 60.0)
-            return sec.toMMSS();
-        return sec;
-    };
-
 
     $scope.calculate = function () {
         var cr = parseFloat($scope.chainSlider);
         var cog = parseFloat($scope.cogSlider);
         var cad = parseFloat($scope.cadenceSlider);
 
-        $scope.gearInches = $scope.inches(cog, cr).toFixed(2);
-        $scope.gearRatio  = $scope.ratio(cog, cr).toFixed(2);
-        $scope.gearSpeed  = $scope.kmh(cog, cr, cad).toFixed(2);
-        $scope.gearTt     = $scope.tt(200.0, cog, cr, cad);
-        $scope.gear400    = $scope.tt(400.0, cog, cr, cad);
-        $scope.gear500    = $scope.tt(500.0, cog, cr, cad);
-        $scope.gear1000   = $scope.tt(1000.0, cog, cr, cad);
+        var g = new Gear(cog, cr);
+
+        $scope.gearInches = g.inches().toFixed(2);
+        $scope.gearRatio  = g.ratio().toFixed(2);
+        $scope.gearSpeed  = g.speed_at(cad).toFixed(2);
+        $scope.gearTt     = g.time(cad, 200.0);
+        var equivs        = g.equivalent();
+        $scope.equiv_up   = equivs["up"];
+        $scope.equiv_down = equivs["down"];
 
         $scope.$apply();
     };
